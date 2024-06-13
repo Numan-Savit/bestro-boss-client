c3ebce7a95c0e30a1a376ec199cc0c392ade13da
Abdullah Al Numan <abdullahalnuman7610@gmail.com>
2024-06-13 20:10:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      
+      <h1 className='text-3xl text-blue-400'>Hello Bangladesh</h1>
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/Main.jsx
@@ -0,0 +1,20 @@
+
+import {Outlet} from "react-router-dom";
+
+import Footer from "../Pages/Footer/Footer";
+import Navbar from "../Pages/Shared/Navbar";
+
+const Main = () => { 
+    
+    return(
+        <div>
+
+           <Navbar></Navbar>
+           <Outlet></Outlet>
+           <Footer></Footer>
+            
+        </div>
+   );  
+};
+
+export default Main;
